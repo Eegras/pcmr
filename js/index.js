09b3262faf74f41e585dfcd9678a06421e308415
Ritwik Dutta <ritzymail@gmail.com>
2014-07-14 14:25:04 -0700

    Cleaning code.

--- js/index.js
+++ js/index.js
@@ -2,26 +2,26 @@ if (window.orientation == 0 || window.orientation == 180) {
     $(".cycle").css("opacity", 0);
 }
 
-jQuery(window).bind('orientationchange', function(e) {
+$(window).bind('orientationchange', function (e) {
 
-  switch ( window.orientation ) {
+    switch (window.orientation) {
 
     case 0:
         $(".cycle").css("opacity", 0);
-    break;
+        break;
 
     case 180:
         $(".cycle").css("opacity", 0);
-    break;
+        break;
 
     case 90:
         $(".cycle").css("opacity", 1);
-    break;
+        break;
 
     case -90:
         $(".cycle").css("opacity", 1);
-    break;
+        break;
 
-  }
+    }
 
 });
