a5b60c307d886ec61d81debc0245310c79a754b4
Ritwik Dutta <ritzymail@gmail.com>
2014-07-14 23:55:00 -0700

    Test

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ $(".link").hover(function () {
     $(".label").text($(this).attr("data-type"));
     $(".label").css("opacity", "1");
 }, function () {
+    $(".label").text("");
     $(".label").css("opacity", "0");
 });
-
 
 if (window.orientation != undefined) {
     $(".redshow").css("display", "none");
