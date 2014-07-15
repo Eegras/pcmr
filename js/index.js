74d20c94fedcd0670d245b3a6296ec99b9678a2e
Ritwik Dutta <ritzymail@gmail.com>
2014-07-14 17:34:26 -0700

    Fixing code formatting.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-$(".link").hover(function() {
+$(".link").hover(function () {
     $(".label").text($(this).attr("data-type"));
     $(".label").css("opacity", "1");
-}, function() {
+}, function () {
     $(".label").css("opacity", "0");
 });
 
@@ -9,26 +9,26 @@ if (window.orientation == 0 || window.orientation == 180) {
     $(".cycle").css("opacity", 0);
 }
 
-jQuery(window).bind('orientationchange', function(e) {
+$(window).bind('orientationchange', function (e) {
 
-  switch ( window.orientation ) {
+    switch (window.orientation) {
 
     case 0:
         $(".cycle").css("opacity", 0);
-    break;
+        break;
 
     case 180:
         $(".cycle").css("opacity", 0);
-    break;
+        break;
 
     case 90:
         $(".cycle").css("opacity", 1);
-    break;
+        break;
 
     case -90:
         $(".cycle").css("opacity", 1);
-    break;
+        break;
 
-  }
+    }
 
 });
